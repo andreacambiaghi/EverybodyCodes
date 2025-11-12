158c16345676e2dc8b812d53ce22a2cdcfc22a20
Andrea Cambiaghi <andrealuigi.cambiaghi@studenti.unimi.it>
2025-11-12 14:41:15 +0100

    Day07

---
+++ EverybodyCode2025/src/ebc2025/Day07.java
@@ -0,0 +1,77 @@
+package ebc2025;
+
+import java.util.*;
+
+public class Day07 extends EBCUtils {
+
+    private Map<String, List<String>> rules;
+    private List<String> names;
+
+    public Day07() { super("7"); }
+
+    @Override
+    void solve(List<String> part1, List<String> part2, List<String> part3) {
+
+        readNamesRules(part1);
+        for(String name : names) {
+            if(isValidName(name)) {
+                solution(name);
+                break;
+            }//if
+        }//for
+
+        readNamesRules(part2);
+        int countValid = 0;
+        for(int i = 0; i < names.size(); i++)
+            if(isValidName(names.get(i)))
+                countValid += i+1;
+        solution(countValid);
+
+        readNamesRules(part3);
+        Set<String> validNames = new HashSet<>();
+        for(String name : names)
+            if(isValidName(name))
+                compose(validNames, name);
+        validNames = new HashSet<>(validNames.stream().filter(name -> name.length() >= 7).toList());
+        solution(validNames.size());
+
+    }//solve
+
+    private void readNamesRules(List<String> input) {
+        
+        names = List.of(input.getFirst().split(","));
+        rules = new HashMap<>();
+        for(int i = 2; i < input.size(); i++) {
+            String[] splitted = input.get(i).split(" > ");
+            rules.put(splitted[0], List.of(splitted[1].split(",")));
+        }//for
+
+    }//readNamesRules
+
+    private boolean isValidName(String name) {
+
+        boolean valid = true;
+        for(int i = 0; i < name.length()-1; i++) {
+            if(!rules.get(name.charAt(i) + "").contains(name.charAt(i + 1) + "")) {
+                valid = false;
+                break;
+            }//if
+        }//for
+        return valid;
+
+    }//isValidName
+
+    void compose(Set<String> validNames, String name) {
+
+        if(name.length() > 11)
+            return;
+
+        validNames.add(name);
+        String last = name.charAt(name.length()-1) + "";
+        List<String> valids = rules.getOrDefault(last, new ArrayList<>());
+        for(String valid : valids)
+            compose(validNames, name + valid);
+
+    }//compose
+
+}//class
--- EverybodyCode2025/src/ebc2025/EBC2025.java
+++ EverybodyCode2025/src/ebc2025/EBC2025.java
@@ -7,6 +7,7 @@ public class EBC2025 {
         //new Day03();
         //new Day04();
         //new Day05();
-        new Day06();
+        //new Day06();
+        new Day07();
     }//main
 }//class
